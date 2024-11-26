0c997c17d2fc4fe70026596c457064bf39c3c54b
pqmEngr <engineer.pqm@gmail.com>
2024-11-26 17:09:17 +0700

    Update main.py

--- seer_system/main.py
+++ seer_system/main.py
@@ -1,14 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from asgiref.wsgi import WsgiToAsgi
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
+api = Api(
+    app, title="My API", description="This is a sample API with Swagger documentation"
+)
 
 
-@app.route("/api/add_message/<uuid>", methods=["GET", "POST"])
-def add_message(uuid):
-    content = request.json
-    print(content["mytext"])
-    return jsonify({"uuid": uuid})
+@app.route("/")
+def home():
+    return "Hello, Flask with Uvicorn!"
 
+
+@api.route("/hello")
+class HelloWorld(Resource):
+    def get(self):
+        """Returns a simple Hello World message"""
+        return {"message": "Hello, World!"}
+
+
+# Wrap the WSGI app to ASGI
+asgi_app = WsgiToAsgi(app)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    import uvicorn
+
+    uvicorn.run(asgi_app, host="0.0.0.0", port=8000)
